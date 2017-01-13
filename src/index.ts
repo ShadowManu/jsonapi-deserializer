d521c129798e57b1b380d8c991ceef142eb6f753
Manuel Pacheco <manuelalejandropm@gmail.com>
2017-01-13 11:41:31 -0400

    add signatures of internal functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+import { Document, Resource, ResourceIdentifier } from './interfaces';
 
-import { Document } from './interfaces';
+export function deserialize(doc: Document): any {
+  let resources = getResources(doc);
+  let relationships = getRelationships(doc);
+  return denormalize(resources, relationships);
+}
 
-export function deserialize(data: Document): any {
- return data;
+interface ResourceCombinator {
+  resource: any;
+  relations: { [name: string]: ResourceIdentifier };
+}
+
+interface RelationshipHash {
+ [type: string]: { [id: string]: Resource };
+}
+
+function getResources(doc: Document): ResourceCombinator[] {
+  // TODO IMPLEMENT
+  return undefined as any;
+}
+
+function getRelationships(doc: Document): RelationshipHash {
+  // TODO IMPLEMENT
+  return undefined as any;
+}
+
+function denormalize(resources: ResourceCombinator[], relations: RelationshipHash): any {
+  // TODO IMPLEMENT
+  return undefined as any;
 }
